a6fda3fe5e67f7dd064b081b8b1bdc8881777075
Alona Harnyk <alyonagarnyk@gmail.com>
2023-03-15 17:29:42 +0200

    222

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import '../css/styles.scss';
+
 import FetchApiService from './fetchCountries';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
